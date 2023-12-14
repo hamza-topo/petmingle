79eb3956fdcd2726a8ad0d54c1843479fcc12dad
Topo <hamzaaitsidisaid.11@gmail.com>
2023-12-15 00:07:16 +0100

    styling css home page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+Echo.channel(`new-match`)
+    .listen('.new.match', (e) => {
+        console.log(e);
+    });
+Echo.channel(`new-message`)
+    .listen('.new.message', (e) => {
+        console.log(e);
+    });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,102 +4,76 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pet Dating Coming Soon</title>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-            background: linear-gradient(to bottom right, #FAF3F0, #DBC4F0);
-            font-family: 'Arial', sans-serif;
-            animation: gradientAnimation 10s infinite alternate;
-        }
-
-        .container {
-            text-align: center;
-            background: linear-gradient(to top left, #FAF3F0, #effaff);
-            padding: 20px;
-            border-radius: 10px;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-            animation: containerAnimation 4s ease-out infinite;
-        }
-
-        h1 {
-            color: #D4E2D4;
-            animation: textAnimation 3s ease-in-out infinite;
-        }
-
-        p {
-            color: #FFCACC;
-            animation: textAnimation 3s ease-in-out infinite;
-        }
-
-        .countdown {
-            color: #DBC4F0;
-            font-size: 1.5em;
-            animation: textAnimation 3s ease-in-out infinite;
-        }
-
-        @keyframes gradientAnimation {
-            0% {
-                background-position: 0% 0%;
-            }
-
-            100% {
-                background-position: 100% 100%;
-            }
-        }
-
-        @keyframes containerAnimation {
-            0% {
-                transform: scale(1);
-            }
-
-            50% {
-                transform: scale(1.1);
-            }
-
-            100% {
-                transform: scale(1);
-            }
-        }
-
-        @keyframes textAnimation {
-
-            0%,
-            100% {
-                transform: scale(1);
-            }
-
-            50% {
-                transform: scale(1.1);
-            }
-        }
-    </style>
+    <title>{{ config('app.name') }}</title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 </head>
 
 <body>
-    <div class="container">
-        comming soon
+    <div class="simple-spinner">
+        <span></span>
     </div>
-    <script>
-        // You can add your countdown script here
-        // For simplicity, a placeholder "30 days" is used
-    </script>
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+    </nav>
+    <div class="container">
+        <div class="app">
+            <img src="{{ asset('img/white.png') }}" alt="" width="150" height="150">
+            <br>
+            <button class="btn">Download</button>
+            <div class="download">
+                <img src="https://static.cdninstagram.com/rsrc.php/v3/yr/r/093c-DX36-y.png" width="100"
+                    height="40">
+                <img src="https://static.cdninstagram.com/rsrc.php/v3/yk/r/NtqqucWkedn.png" width="100"
+                    height="40">
+            </div>
+        </div>
+        <div class="hero-section">
+            <div class="slides">
+                <input type="radio" name="slide" id="c1">
+                <label for="c1" class="card">
+                    <div class=row>
+                        <div class="icon">
+                            < </div>
+                                <div class="description">
+
+                                </div>
+                        </div>
+                </label>
+                <input type="radio" name="slide" id="c2">
+                <label for="c2" class="card">
+                    <div class=row>
+                        <div class="icon"></div>
+                        <div class="description">
+                        </div>
+                    </div>
+                </label>
+                <input type="radio" name="slide" id="c3">
+                <label for="c3" class="card">
+                    <div class="card-title-right">
+                        <h1>Lorem Ipsum</h1>
+                    </div>
+                    <div class=row>
+                        <div class="icon"></div>
+                        <div class="description">
+                        </div>
+                    </div>
+                </label>
+                <input type="radio" name="slide" id="c4" checked>
+                <label for="c4" class="card">
+                    <div class="card-title-left">
+                        <h1>Lorem Ipsum</h1>
+                    </div>
+                    <div class=row>
+                        <div class="icon">></div>
+                        <div class="description">
+                        </div>
+                    </div>
+                </label>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
-<script src="{{ asset('js/app.js') }}"></script>
-<script>
-    Echo.channel(`new-match`)
-        .listen('.new.match', (e) => {
-            console.log(e);
-        });
-    Echo.channel(`new-message`)
-        .listen('.new.message', (e) => {
-            console.log(e);
-        });
-</script>
