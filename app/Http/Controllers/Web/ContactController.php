d3e941a99ceec22ff7207ed10f3f47602b28a819
YoussefTamri <yousseftam100@gmail.com>
2024-09-08 13:52:28 +0000

    Added validation form contact and Added detailed error handling to display validation errors

--- app/Http/Controllers/Web/ContactController.php
+++ app/Http/Controllers/Web/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Events\Contact as ContactEvent;
+use App\Http\Requests\Web\Contact\Store;
 use App\Mail\Contact;
 use App\Models\Contact as ModelsContact;
 use Illuminate\Http\Request;
@@ -29,16 +30,18 @@ class ContactController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Store $request)
     {
         //TODO:validate request
+        //validate request is done
         try {
             Log::info('send email contact ...');
             Mail::to(env('MAIL_CONTACT_US'))->queue(new Contact($request->all()));
             ModelsContact::create($request->all());
 
-            return redirect(route('contact'));
+            return redirect(route('contact'))->with('success', __('Your message has been sent successfully!'));
         } catch (\Exception $e) {
+            return redirect()->back()->with('error', __('Failed to send your message. Please try again later.'));
             Log::error('failed send email contact..:[ ' . $e->getMessage() . ' ]');
         }
     }
