27cfb6e8ef198674a12024ee6f1d3195558707f4
YoussefTamri <yousseftam100@gmail.com>
2024-09-10 11:50:43 +0000

    make seeder for newslatter

--- app/Http/Controllers/Web/BlogController.php
+++ app/Http/Controllers/Web/BlogController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\NewsLetterRepository;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
+    public function __construct(protected NewsLetterRepository $newsRepository) {}
+
     public function __invoke()
     {
-        return view('web.blog');
+        $news = $this->newsRepository->all();
+        return view('web.blog', compact('news'));
     }
 }
