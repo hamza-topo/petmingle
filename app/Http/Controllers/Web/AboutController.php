ff6657727df91c7a5ccc4fb0d43d90a6823c461f
Topo <hamzaaitsidisaid.11@gmail.com>
2024-11-10 11:34:53 +0100

    About us component

--- app/Http/Controllers/Web/AboutController.php
+++ app/Http/Controllers/Web/AboutController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers\Web;
 
 use App\Enums\Pages;
-use App\Http\Controllers\Controller;
 use App\Repositories\SeoRepository;
+use App\Http\Controllers\Controller;
+use App\Repositories\BlogRepository;
 
 class AboutController extends Controller
 {
-    public function __construct(protected SeoRepository $seoRepository) {}
+    public function __construct(
+        protected SeoRepository $seoRepository,
+        protected BlogRepository $blogRepositoryRepository,
+    ) {}
     public function __invoke()
     {
+        $slug = __('about');
         $seo = $this->seoRepository->getByKey(Pages::ABOUT->value);
-        
-        return view('web.about', compact('seo'));
+        $blog = $this->blogRepositoryRepository->getBySlug($slug, app()->getLocale());
+
+        return view('web.about', compact('seo', 'blog'));
     }
 }
