5a9c8741ce355fca602db58cf49f353b8887f278
Topo <hamzaaitsidisaid.11@gmail.com>
2024-11-17 22:53:38 +0100

    click on image redirect to blog

--- resources/views/web/blog.blade.php
+++ resources/views/web/blog.blade.php
@@ -18,17 +18,21 @@
                         {!! generateTextPreview($blogs->first()->content[app()->getLocale()] ?? '') !!}
 
                         <div class="section-button mt-27 d-inline-block">
-                            @if(!empty($blogs->first()->slug[app()->getLocale()]))
-                            <a href="{{ route('blogs.read', $blogs->first()->slug[app()->getLocale()]) }}"
-                                class="btn-primary-icon-sm radius-20">
-                                <p class="pera mt-0">Learn More</p>
-                                <i class="ri-arrow-right-up-line"></i>
-                            </a>
+                            @if (!empty($blogs->first()->slug[app()->getLocale()]))
+                                <a href="{{ route('blogs.read', $blogs->first()->slug[app()->getLocale()]) }}"
+                                    class="btn-primary-icon-sm radius-20">
+                                    <p class="pera mt-0">Learn More</p>
+                                    <i class="ri-arrow-right-up-line"></i>
+                                </a>
                             @endif
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-7 col-lg-6">
+                @php
+                    $url = route('blogs.read', $blogs->first()->slug[app()->getLocale()] ?? '');
+                @endphp
+                <div class="col-xl-7 col-lg-6"
+                    onclick="event.preventDefault(); window.location.href='{{ $url }}';">
                     <div class="about-count-section about-count-before-bg">
                         <div class="banner">
                             @php
@@ -39,7 +43,9 @@
                         </div>
                         <div class="all-count-list">
                             <div class="details">
-                                <p class="pera">{{ readTimesStamps($blogs->first()->created_at ?? Illuminate\Support\Carbon::now()) }}</p>
+                                <p class="pera">
+                                    {{ readTimesStamps($blogs->first()->created_at ?? Illuminate\Support\Carbon::now()) }}
+                                </p>
                             </div>
                             <div class="divider"></div>
                             <div class="details">
@@ -68,7 +74,7 @@
                 </div>
             </div>
             <x-web.home.blog.side-section :blogs="$blogs->skip(1)->take(6)" />
-                {{$blogs->links('vendor.pagination.default')}}
+            {{ $blogs->links('vendor.pagination.default') }}
         </div>
     </section>
 @endsection
