f57b699c1898cc1018c85e628e45350bca9639fd
Topo <hamzaaitsidisaid.11@gmail.com>
2024-11-19 19:31:02 +0100

    Add pinterest board widget

--- resources/views/components/web/layout/footer.blade.php
+++ resources/views/components/web/layout/footer.blade.php
@@ -3,12 +3,12 @@
         <div class="container">
             <div class="footer-area">
                 <div class="row g-4">
-                    <div class="col-xl-6 col-lg-6 col-sm-6">
+                    {{-- <div class="col-xl-6 col-lg-6 col-sm-6">
                         <div class="single-footer-caption">
                             <div class="footer-tittle">
                                 <h4 class="title">{{ __('Quick Links') }}</h4>
                                 <ul class="listing">
-                                    <li class="single-lsit"><a href="{{ url('/') }}">{{ __('Home') }}</a></li>
+                                    <li class="single-lsit"></li>
                                     <li class="single-lsit"><a href="{{ route('blogs') }}">{{ __('Magazine') }}</a></li>
                                     <li class="single-lsit">
                                         <a href="{{ route('about') }}">{{ __('About Us') }}</a>
@@ -19,33 +19,13 @@
                                 </ul>
                             </div>
                         </div>
-                    </div>
+                    </div> --}}
                     <div class="col-xl-6 col-lg-6 col-sm-6">
                         <div class="single-footer-caption">
+                            <a data-pin-do="embedBoard" data-pin-board-width="400" data-pin-scale-height="240"
+                                data-pin-scale-width="80"
+                                href="https://www.pinterest.com/petspat/adopt-dont-shop-find-your-furry-friend"></a>
                             <div class="footer-tittle">
-                                <h4 class="title">{{ __('Contact') }}</h4>
-                                <ul class="listing">
-                                    <li class="single-lsit">
-                                        <a href="#" class="mb-20 d-block">420 Pittsburg Landing,
-                                            Summerville, South Carolina, 29483</a>
-                                    </li>
-                                    <li class="single-lsit">
-                                        <a href="#">
-                                            <div class="d-flex gap-12">
-                                                <i class="ri-phone-line"></i>
-                                                {{ $component->content['phone']['value'] ?? '' }}
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li class="single-lsit">
-                                        <a href="#">
-                                            <div class="d-flex gap-12">
-                                                <i class="ri-mail-line"></i>
-                                                {{ $component->content['email']['value'] ?? '' }}
-                                            </div>
-                                        </a>
-                                    </li>
-                                </ul>
                             </div>
                         </div>
                     </div>
@@ -63,9 +43,9 @@
                             <h4>{{ __('Stay Informed with the Latest Pet Health Tips!') }}</h4>
                             <p class="pera">
                                 {{ __('Don’t miss out on expert advice, pet care tips, and exclusive updates tailored for pet
-                                                                                                                                                                                                parents like you. Subscribe to our newsletter to receive the latest on pet health
-                                                                                                                                                                
-                                                                                                                                                                                                Join Our Pet Parent Community Today!') }}
+                                                                                                                                                                                                                                                                parents like you. Subscribe to our newsletter to receive the latest on pet health
+                                                                                                                                                                                                                                
+                                                                                                                                                                                                                                                                Join Our Pet Parent Community Today!') }}
                             </p>
                         </div>
                         <div class="footer-right">
@@ -92,16 +72,20 @@
                             <div class="col-md-4 col-lg-4">
                                 <p class="pera">
                                     © <span class="current-year">{{ date('Y-m-d') }}</span>.
-                                    {{ __('All rights reserved') }}
+                                    {{ __('All rights reserved') }}. {{ __('Powered by') }} {{ config('app.name') }}
                                 </p>
                             </div>
                             <div class="col-md-4 col-lg-4">
-                                <p class="pera">{{ __('Powered by') }} {{ config('app.name') }}</p>
+                                <a class="pera" href="{{ route('privacy-policy') }}">{{ __('Privacy Policy') }}</a>
                             </div>
-                            <div class="col-md-4 col-lg-4">
-                                <a class="pera" href="{{route('privacy-policy')}}">{{ __('Privacy Policy') }}</a>
+                            <div class="col-md-4 col-lg-4 row">
+                                <a class="pera" href="{{ url('/') }}">{{ __('Home') }}</a>
+                                <a class="pera" href="{{ route('about') }}">{{ __('About Us') }}</a>
+                                <a class="pera" href="{{ route('blogs') }}">{{ __('Magazine') }}</a>
+                                <a class="pera" href="{{ route('contact') }}">{{ __('Contact Us') }}</a>
                             </div>
-                            <div class="row col-md-12 col-lg-12">
+                            <div class="row col-md-12 col-lg-12"
+                                style="display: flex;flex-direction:row;justify-content:center">
                                 <a href="{{ config('app.pinterest_url') }}" target="_blank">
                                     <i class="ri-pinterest-fill" style="font-size: 2rem;"></i>
                                 </a>
